1f14e83c03ad299e4ef9c838cdc26ca6347fbc65
eavc1821 <e.alejocarcamo10@gmail.com>
2025-10-27 17:39:33 -0600

    nuevo server

--- server.js
+++ server.js
@@ -104,7 +104,7 @@ const initDB = async () => {
     await sequelize.authenticate();
     console.log("✅ Conectado a PostgreSQL");
 
-    await sequelize.sync({ alter: true });
+    await sequelize.sync();
     console.log("🧩 Tablas sincronizadas correctamente");
 
     await sequelize.query('CREATE INDEX IF NOT EXISTS idx_employees_dni ON "Employees" (dni);');
